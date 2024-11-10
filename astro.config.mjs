690119a79753ccd97f9d53449d214ba9e8bd76f2
Scrrrr <Scrrrr@users.noreply.github.com>
2024-11-11 02:49:14 +0900

    fixed astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fuwari.vercel.app/',
-  base: '/',
+  site: 'https://scrrrr.github.io/',
+  base: '/fuwari',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
